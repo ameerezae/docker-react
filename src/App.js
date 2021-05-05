b1c87033494de7bbbbafa19eb62a2f8c8ad4b19b
amirrezaeii <amir.rezaei1999@yahoo.com>
2021-05-05 16:15:01 +0430

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test project for continues integration using Travis-Ci and continues deployment using AWS
         </p>
         <a
           className="App-link"
